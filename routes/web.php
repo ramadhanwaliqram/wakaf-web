4a71ece43a5e286bd6c608d553790aac43dceaa8
Ramadhan Wal Iqram <ramadhanwaliqram@gmail.com>
2023-06-01 00:10:45 +0700

    multi authentication

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -31,7 +31,7 @@ Route::middleware('auth')->group(function () {
 
 Route::namespace('admin')
     ->name('admin.')
-    // ->middleware(['can:isAdmin', 'auth'])
+    ->middleware(['auth', 'role:admin'])
     ->group(
         function () {
             Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name(
